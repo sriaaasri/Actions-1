60fae5c0475f5252b00d0037532ddd4da19c3176
sri_satya <91309599+sriaaasri@users.noreply.github.com>
2026-03-27 16:49:28 +0530

    Added multiple jobs

--- run-ascii.sh
+++ run-ascii.sh
@@ -2,5 +2,5 @@
 echo "Executing the run-ascii shell script"
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am DRAGON..........." >> dragon.txt
-ll -a
-cat dragon.txt
+# ll -a
+# cat dragon.txt
